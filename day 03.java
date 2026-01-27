c1a8209d667ee43be48122996af4706b652a3166
Aditya152602 <adityadwivedi26oct@gmail.com>
2026-01-27 14:13:27 +0530

    Add files via upload

---
+++ day 03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class ThirdClass {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+        String name = sc.nextLine();
+        System.out.println(name);}
+    }
+    
+}

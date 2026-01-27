5d4007982360ef06903ff288ae7debf81f4bf6c4
Aditya152602 <adityadwivedi26oct@gmail.com>
2026-01-27 14:13:00 +0530

    Add files via upload

---
+++ day 02.java
@@ -0,0 +1,17 @@
+class SecondClass  {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum =a+b;
+        System.out.println(sum);
+        int diff = a-b;
+        System.out.println(diff);
+        int mult = a*b;
+        System.out.println(mult);
+        int ans=(a*b)/(a-b);
+        System.out.println(ans);
+
+    }
+
+    
+}

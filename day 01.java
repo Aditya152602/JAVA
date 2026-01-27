bf6a51c3297be4c8535c9832de35e997e3ca25c4
Aditya152602 <adityadwivedi26oct@gmail.com>
2026-01-27 14:12:31 +0530

    Add files via upload

---
+++ day 01.java
@@ -0,0 +1,12 @@
+/*class FirstClass {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    
+        }
+}*/
+import java.lang.*;
+public class Morning{
+    public static void main(String[] args){
+        System.out.println("Good Morning");
+    }
+}

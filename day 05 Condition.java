855c011d2623966ae9babc1e82868b382697273d
Aditya152602 <adityadwivedi26oct@gmail.com>
2026-01-28 18:10:17 +0530

    Add files via upload

---
+++ day 05 Condition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Conditions{
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+        int age = sc.nextInt();
+        if (age > 18) {
+            System.out.println("Adult");
+
+        } else{
+            System.out.println("Not Adult");
+        }}
+
+    }
+
+}
+

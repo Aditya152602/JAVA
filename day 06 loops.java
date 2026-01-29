8110065dff512be8da6b76b18fdd43d691b49cb9
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-01-29 18:35:33 +0530

    Add files via upload

---
+++ day 06 loops.java
@@ -0,0 +1,46 @@
+/*import java.util.*;
+class Conditions {
+  public static void main(String[] args) {
+       int i=0;
+        while(i<11){
+         System.out.println(i);
+            i++;
+        }
+
+  }
+
+
+
+
+                                      For calculating the sum of n natural no.
+
+
+
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+       int n = sc.nextInt();
+       int sum = 0;
+      for (int i=0;i<=n;i++){
+          sum=sum+i;
+          System.out.println(sum);
+      }
+    }
+
+                                           For printing any desire table.
+
+
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=1;i<=10;i++){
+            System.out.println(i*n);
+
+      }
+    }
+
+
+  }
+}*/
+

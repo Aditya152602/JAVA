fcbd8cbabc0641a9940093d17622b6f6d4d95d29
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-02-01 09:02:07 +0530

    Add files via upload

---
+++ day09Functions.java
@@ -0,0 +1,60 @@
+/*import java.util.*;
+public class day09Functions{
+//    public static void printMyName(String name){
+//        System.out.println(name);
+//        return;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+
+//        printMyName(name);
+
+// Que: Make a function to add 2 numbers and return the sum.
+
+//public static int calculateSum(int a, int b){
+//        int sum = a+b;
+//        return sum;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int a= sc.nextInt();
+//        int b= sc.nextInt();
+//        int sum = calculateSum(a,b);
+//        System.out.println("Sum of a two numbers is : "+ sum);
+
+// Que: Make a function to multiply two numbers and return the product.
+
+//    public static int calculateProduct(int a, int b){
+//        return a*b;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int a= sc.nextInt();
+//        int b= sc.nextInt();
+//       System.out.println("Product of a two numbers : "+ calculateProduct(a,b));
+
+    
+// Que: Find the factorial of a number.
+     public static void printFactorial(int n){
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
+        int factorial = 1;
+        for (int i=n; i>=1; i--){
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        printFactorial(n);
+
+    }
+   
+}*/
+

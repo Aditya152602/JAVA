361b5f9a2ca05128ccccd7bc721006dbfc3ff04e
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-02-06 08:44:39 +0530

    Add files via upload

---
+++ day14.java
@@ -0,0 +1,18 @@
+/*import java.util.*;
+public class day14 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first no. :");
+        int a = sc.nextInt();
+        System.out.println("Enter the second no. :");
+        int b = sc.nextInt();
+        System.out.println(a<b);
+        System.out.println(a+b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a%b);
+        System.out.println(a*b);
+        System.out.println(7*49/7+35/7);
+
+    }
+}*/

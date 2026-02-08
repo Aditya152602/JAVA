d2d0e7e01a8fc39ddea10081424c175855d92ce3
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-02-08 08:53:29 +0530

    Add files via upload

---
+++ day16Sorting.java
@@ -0,0 +1,64 @@
+public class day16Sorting {
+//                                                              BUBBLE SORT
+
+//     public static void printArray(int arr[]){
+//        for (int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+//        System.out.println();
+//    }
+//    public static void main(String args[]) {
+//        int arr[] = {7,8,9,5,3,4,2,1,6};
+
+        // Time complexity = O(n^2)
+        //Bubble sort
+//        for (int i=0; i<arr.length-1; i++){// n-1
+//            for (int j=0; j<arr.length-i-1; j++){
+//                if(arr[j] > arr[j+1]){
+                    //swap
+//                    int temp=arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//            }
+//        }
+
+//        printArray(arr);
+//    }
+
+//                                                               SELECTION SORT
+
+//    public static void printArray(int arr[]){
+//        for (int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+//        System.out.println();
+//    }
+//    public static void main(String args[]) {
+//        int arr[] = {7,8,9,5,3,4,2,1,6};
+
+        // Time complexity = O(n^2)
+        //Selection sort
+//        for (int i=0; i<arr.length-1; i++){// n-1
+//            int smallest = i;
+//            for (int j=i+1; j<arr.length; j++) {
+//                if (arr[smallest] > arr[j]) {
+//                    smallest = j;
+//                }
+//            }
+                     //swap
+//                    int temp=arr[smallest];
+//                    arr[smallest] = arr[i];
+//                    arr[i] = temp;
+
+
+//        }
+//        printArray(arr);
+
+//    }
+
+    
+}
+    
+    
+

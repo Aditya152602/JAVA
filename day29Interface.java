950040297108e9e4192df27c98aa56f0cd6cb730
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-03-15 09:05:12 +0530

    Add files via upload

---
+++ day29Interface.java
@@ -0,0 +1,18 @@
+/*interface Animal{
+    int eyes = 2;
+     void walk();
+}
+interface Herbivores{
+
+}
+class Horse implements Animal,Herbivores{
+    public void walk(){
+        System.out.println("Walks on legs");
+    }
+}
+public class day29Interface {
+    public static void main (String args[]){
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}*/

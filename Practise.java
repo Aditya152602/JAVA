ac688076a375d585b93949ca90c2319a77a8bb78
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-03-17 17:47:31 +0530

    Add files via upload

---
+++ Practise.java
@@ -0,0 +1,212 @@
+/*import java.util.*;
+public class Practise {
+  public static void main(String[] args) {
+
+//        System.out.println("The result of an half yearly exam is given below :");
+//       Scanner sc= new Scanner(System.in);
+//       System.out.println("Enter the marks of Mathematics :");
+//       int a = sc.nextInt();
+//        System.out.println("Enter the marks of Hindi :");
+//       int b = sc.nextInt();
+//        System.out.println("Enter the marks of English :");
+//       int c = sc.nextInt();
+//        System.out.println("Enter the marks of computer science :");
+//        int d = sc.nextInt();
+//        System.out.println("Enter the marks of science");
+//        int e = sc.nextInt();
+//        float percentage = ((a+b+c+d+e)/500.0f)*100;
+//        System.out.println("Percentage :");
+//        System.out.println(percentage);
+
+
+// }
+
+
+
+//}
+
+//                                                           GRETS
+
+
+//
+//  public static void main(String[] args) {
+//    System.out.println("What is your name :");
+//  Scanner sc = new Scanner(System.in);
+//String name = sc.nextLine();
+//System.out.println("Hello "+name+" have a good day!");
+
+
+
+// }
+
+
+
+//
+
+//                                                        CONVERTING KM TO MILES 
+
+//import java.util.*;
+//class Conditions {
+//    public static void main(String[] args) {
+//        double km ,miles;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the distance in km :");
+//        km = input.nextDouble();
+//        input.close();
+//        miles = (km*0.621371);
+//       System.out.printf("%2f kilometers =%2f Miles",km,miles);
+
+
+
+//}
+
+
+
+//}
+
+//                                   TO CHECK WHETHER THE ENTERED NO. IS INTEGER OR NOT.
+
+//import java.util.*;
+//class Conditions {
+//    public static void main(String[] args) {
+//        System.out.println("Enter the number :");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(sc.hasNextInt());
+
+
+
+//    }
+
+
+
+//}
+
+
+//                                                  ESCAPE SEQENCE CHARACTERS
+
+//import java.util.*;
+//class  Conditions{
+//    public static void main(String[] args){
+//            String myletter = "Dear Aditya,\n\tDwivedi.\n\tanuj";
+//            System.out.println(myletter);
+//    }
+//}
+
+//                                                CONDITIONALS EXAMPLE IN JAVA
+
+Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+          if (n%2==0) {
+                System.out.println("Is an even number");
+
+            } else{
+                System.out.println("Is not an even number");
+            }
+            
+      // NESTED IF ELSE
+    int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a==b) {
+                System.out.println("They both are equal.");
+
+            } else if (a>b) {
+                System.out.println("N is greater then m. ");
+
+            } else{
+                System.out.println("N is lesser.");
+            }
+
+            // SWITCH CASE EXAMPLE
+    int button = sc.nextInt();
+        switch (button){
+            case 1 :
+                System.out.println("Hello");
+            break;
+            case 2 :
+                System.out.println("Namaste");
+            break;
+            case 3 :
+                System.out.println("Bonjour");
+            break;
+            default:
+                System.out.println("Invalid Button");
+        }
+  int day = sc.nextInt();
+switch (day){
+  case 1 : 
+      System.out.println("Monday");
+  break;    
+  case 2 :
+      System.out.println("Tuesday");
+  break;
+  case 3 : 
+      System.out.println("Wednesday");
+  break;
+  case 4 : 
+      System.out.println("Thursday");
+  break;
+  case 5 : 
+      System.out.println("Friday");
+  break;
+  case 6 : 
+      System.out.println("Saturaday");
+  break;
+  case 7 : 
+      System.out.println("Sunday");
+  break;
+  default :
+  System.out.println("Invalid");
+}
+
+//                                     WAP TO FIND OUT THE TYPE OF WEBSITE FROM THE URL
+
+  String website = sc.next();
+if (website.endsWith(".org")) {
+  System.out.println("This is an organistional website");
+}
+else if(website.endsWith(".in")){
+  System.out.println("This is an Industial Website");
+
+}
+  // WAP TO FIND FACTORIAL OF A GIVEN NO. USING FOR LOOP
+  int c = 5;
+  int factorial = 1;
+  for(int i = 1; i<=c;i++){
+    factorial*=i;
+  }  System.out.println(factorial);
+
+   //                 WAP TO CALCULATE THE SUM OF THE NUMBERS OCCURING IN THE ANY MULTIPLICATION TABLE
+    int o =sc.nextInt();
+        int sum = 0;
+        for(int i=1;i<=10;i++){
+            sum += o*i;
+        }
+        System.out.println(sum);
+    }
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

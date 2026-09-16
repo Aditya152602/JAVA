7805ffd5ba6af75cf0581d2d833fe4368ac5e223
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-09-16 19:11:54 +0530

    Update Practise.java

--- Practise.java
+++ Practise.java
@@ -411,6 +411,19 @@ public class Palindrome {
     }
 }
 
+import java.util.Scanner;
+public class ReverseEveryWordInSentenceString {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence");
+        String a = sc.nextLine();
+        System.out.println("Reversed");
+        for (int i = a.length()-1;i>=0;i--){
+            System.out.println(a.charAt(i));
+        }
+    }
+
+}
 
 
 

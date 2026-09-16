a1990de1d8995446da41ad2c73fcbfc2e83d0d5e
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-09-16 19:06:38 +0530

    Update Practise.java

--- Practise.java
+++ Practise.java
@@ -379,7 +379,37 @@ import java.util.Arrays;
 //
 //}
 
+public class Palindrome {
 
+    public static boolean isPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        if (isPalindrome(str)) {
+            System.out.println("Is Palindrome");
+        } else {
+            System.out.println("Is not Palindrome");
+        }
+    }
+}
 
 
 
